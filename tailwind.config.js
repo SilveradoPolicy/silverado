16a4af1853229d4aaed12c245bba0748a3f9f19a
asasmith <smith.asa.la@gmail.com>
2020-12-30 11:39:58 -0500

    add typescale

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ module.exports = {
                 'brand-5': '#A3C1BF',
                 'brand-6': '#FCAF1B',
             },
+            fontSize: {
+                'ts-h1': ['2.986rem', '3rem'],
+                'ts-h2': ['2.488rem', '2.488rem'],
+                'ts-h3': ['2.074rem', '3.11rem'],
+                'ts-h4': ['1.728rem', '2.16rem'],
+                'ts-h5': ['1.44rem', '1.801rem'],
+                'ts-h6': ['1.2rem', '1.5rem'],
+                'ts-body': ['1rem', '1.5rem'],
+                'ts-sub-detail': ['0.833rem', '1.04rem'],
+                'ts-caption': ['0.694rem', '0.868rem'],
+                'ts-citation': ['0.579rem', '0.724rem'],
+            },
         },
     },
     variants: {
