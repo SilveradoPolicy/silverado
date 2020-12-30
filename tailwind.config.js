c3f498c448fca2dad9e0501baf0745acde2315f3
asasmith <smith.asa.la@gmail.com>
2020-12-30 11:39:25 -0500

    add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
     purge: ['./src/**/*.js', './src/**/*.jsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'brand-1': '#306184',
+                'brand-2': '#739663',
+                'brand-3': '#56959E',
+                'brand-4': '#C4E7E7',
+                'brand-5': '#A3C1BF',
+                'brand-6': '#FCAF1B',
+            },
+        },
     },
     variants: {
         extend: {},
