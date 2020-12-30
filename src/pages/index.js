da4c1a87eaa363efe2f7befee713e9d2739930d2
Asa Smith <smith.asa.la@gmail.com>
2020-12-30 17:10:06 -0500

    Brand palette setup (#1)
    * add brand colors
    
    * add typescale
    
    * blank 404 page
    
    * add layout component
    
    * move layout from components/ to layouts/
    
    * add fonts
    
    * add logo svg's and icon png
    
    * remove comments
    
    * update readme, remove backroom references

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+import Layout from '../layouts/page-layout';
+
+const StyledHeading = styled.h1`
+    ${tw`text-ts-citation text-brand-1`}
+`;
+
 export default function IndexPage() {
     return (
-        <>
-            <h1>index page</h1>
-        </>
+        <Layout>
+            <StyledHeading>testing paragraph</StyledHeading>
+        </Layout>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@ module.exports = {
     purge: ['./src/**/*.js', './src/**/*.jsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'brand-1': '#306184',
+                'brand-2': '#739663',
+                'brand-3': '#56959E',
+                'brand-4': '#C4E7E7',
+                'brand-5': '#A3C1BF',
+                'brand-6': '#FCAF1B',
+            },
+            fontSize: {
+                'ts-h1': ['2.986rem', '3rem'],
+                'ts-h2': ['2.488rem', '2.488rem'],
+                'ts-h3': ['2.074rem', '3.11rem'],
+                'ts-h4': ['1.728rem', '2.16rem'],
+                'ts-h5': ['1.44rem', '1.801rem'],
+                'ts-h6': ['1.2rem', '1.5rem'],
+                'ts-body': ['1rem', '1.5rem'],
+                'ts-sub-detail': ['0.833rem', '1.04rem'],
+                'ts-caption': ['0.694rem', '0.868rem'],
+                'ts-citation': ['0.579rem', '0.724rem'],
+            },
+        },
+        fontFamily: {
+            'wt-thin': 'SukhumvitThin',
+            'wt-reg': 'SukhumvitReg',
+            'wt-bold': 'SukhumvitBold',
+        },
     },
     variants: {
         extend: {},
