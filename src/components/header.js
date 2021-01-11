be9b9c3099044cafbfde91b205d519496e2ce3fa
Asa Smith <smith.asa.la@gmail.com>
2021-01-11 16:55:45 -0500

    Header fix (#9)
    * add colors to global css for updating via props
    
    * make header fixed, add bg color on scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import { Link } from 'gatsby';
 
 import Logo from './logos/horizontal';
 
+const StyledHeader = styled.header`
+    ${tw`fixed transition w-full z-10`}
+
+    background-color: ${(props) =>
+        props.isScrolled ? `#ffffff` : `#ffffff00`};
+`;
 const CtaBar = styled.div`
     ${tw`h-10 bg-brand-1`}
 `;
@@ -22,7 +28,9 @@ const StyledUL = styled.ul`
 `;
 
 const StyledLI = styled.li`
-    ${tw`ml-8 capitalize font-wt-bold text-lg text-white`}
+    ${tw`ml-8 capitalize font-wt-bold text-lg transition`}
+
+    color: ${(props) => (props.isScrolled ? `var(--brand-1)` : `var(--white)`)}
 `;
 
 const nav = [
@@ -48,16 +56,34 @@ const nav = [
 // todo: nav hover states, confirm desgin w/ Josh
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 10) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
+
     const navItems = nav.map((item) => {
         return (
-            <StyledLI key={item.text}>
+            <StyledLI key={item.text} isScrolled={isScrolled}>
                 <Link to={item.link}>{item.text}</Link>
             </StyledLI>
         );
     });
 
     return (
-        <header>
+        <StyledHeader isScrolled={isScrolled}>
             <CtaBar />
             <StyledNav>
                 <StyledLogoLink to="/">
@@ -65,6 +91,6 @@ export default function Header() {
                 </StyledLogoLink>
                 <StyledUL>{navItems}</StyledUL>
             </StyledNav>
-        </header>
+        </StyledHeader>
     );
 }
