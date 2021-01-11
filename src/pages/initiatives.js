ae8426a1eaacdb6035ef52f3ed97f0c75beca698
asasmith <smith.asa.la@gmail.com>
2021-01-11 17:08:37 -0500

    create initiatives page

---
+++ src/pages/initiatives.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../layouts/page-layout';
+
+export default function InitiativesPage() {
+    return (
+        <Layout>
+            <h1>test</h1>
+        </Layout>
+    );
+}
