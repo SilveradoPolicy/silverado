5647c2699c0ff5e2722cbb4fb6608d3bafa2b499
Asa Smith <smith.asa.la@gmail.com>
2021-01-11 20:41:32 -0500

    add alternate hero component (#11)

--- src/pages/initiatives.js
+++ src/pages/initiatives.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Layout from '../layouts/page-layout';
 
+import AltHero from '../components/altHero';
+
+const content = {
+    hero: {
+        body: `Cras iaculis, lectus a condimentum lacinia, risus ex varius est, vel fermentum magna enim sed eros. Vestibulum at augue eget turpis pharetra mollis vel sagittis elit. Ut eleifend sodales vehicula. Nam malesuada massa vitae tellus sagittis tincidunt in in sem.`,
+        heading: 'Great Power Competition Headline',
+    },
+};
+
 export default function InitiativesPage() {
     return (
         <Layout>
-            <h1>test</h1>
+            <AltHero body={content.hero.body} heading={content.hero.heading} />
         </Layout>
     );
 }
