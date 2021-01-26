ba9de94340179332372475ad0483e39949b6018b
kcbreuil <kcbreuil@gmail.com>
2021-01-25 22:57:59 -0500

    initiatives update

--- src/components/altHero.js
+++ src/components/altHero.js
@@ -4,15 +4,15 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 const StyledSection = styled.section`
-    ${tw`h-screen md:h-3/4-screen flex flex-col justify-center text-white`}
+    ${tw`h-screen md:h-3/4-screen flex flex-col justify-center text-white mb-20`}
 `;
 
 const Heading = styled.h1`
-    ${tw`font-wt-bold mb-14 text-brand-1 text-center text-ts-h1`}
+    ${tw`font-wt-bold mb-8 mt-44 text-brand-1 text-center text-ts-h1`}
 `;
 
 const Body = styled.p`
-    ${tw`max-w-prose mx-auto text-black text-center text-ts-h4`}
+    ${tw`max-w-prose mx-auto text-black text-center text-ts-h5`}
 `;
 
 export default function AltHero({ body, heading }) {
