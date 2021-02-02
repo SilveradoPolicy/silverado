e6980b797a34ffcf72c3fd642c1eb2a7e8ac71ce
Asa Smith <smith.asa.la@gmail.com>
2021-02-01 22:04:57 -0500

    update padding breakpoint for container elements (#97)

--- src/components/pillars.js
+++ src/components/pillars.js
@@ -17,7 +17,7 @@ const StyledSection = styled.section`
 `;
 
 const Grid = styled.div`
-    ${tw`container px-5 lg:px-0 gap-10 grid grid-cols-1 md:grid-cols-3`}
+    ${tw`container px-5 xl:px-0 gap-10 grid grid-cols-1 md:grid-cols-3`}
 `;
 
 const Card = styled.div`
