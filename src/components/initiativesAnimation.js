53cb20edb2a41c94f612038e01b83d86f0bd99de
asasmith <smith.asa.la@gmail.com>
2021-02-03 08:53:32 -0500

    move initiative animations to animation hook, change file names

--- src/components/initiativesAnimation.js
+++ src/components/initiativesAnimation.js
@@ -1,145 +1,21 @@
-import React from 'react';
-import Lottie from 'lottie-react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
+import { useLottie, useLottieInteractivity } from 'lottie-react';
 
-import accelerate from '../animations/Accelerate.json';
-import cultivate from '../animations/Cultivate.json';
-import icons from '../animations/Icons.json';
-import incubate from '../animations/Incubate.json';
-
-import InitiativesText from './initiativesText';
-
-const InitiativesWrapper = styled.div`
-    ${tw`grid grid-cols-5 grid-rows-4 justify-center`}
-
-    .icons {
-        grid-column-start: 1;
-        grid-column-end: 6;
-        align-self: flex-end;
-    }
-    #cultivate-text {
-        grid-column-start: 3;
-        grid-column-end: 5;
-        align-self: flex-start;
-    }
-    .cultivate {
-        grid-column: 2 / span 1;
-        grid-row-start: 2;
-        grid-row-end: 3;
-    }
-    #incubate-text {
-        grid-column-start: 2;
-        grid-column-end: 4;
-        grid-row-start: 3;
-        grid-row-end: 4;
-    }
-    .incubate {
-        grid-column-start: 4;
-        grid-column-end: 5;
-    }
-    #accelerate-text {
-        grid-column-start: 3;
-        grid-column-end: 5;
-    }
-    .accelerate {
-        grid-column-start: 2;
-        grid-row-start: 4;
-        grid-row-end: 5;
-    }
-`;
-
-export default function InitiativesAnimation() {
-    const style = {
-        height: 300,
+export default function ScrolledAnimation({ data, frames, style }) {
+    const options = {
+        animationData: data,
     };
-
-    const interactivity = {
+    const lottieObj = useLottie(options, style);
+    const Animation = useLottieInteractivity({
+        lottieObj,
         mode: 'scroll',
         actions: [
             {
-                visibility: [0, 0.3],
-                type: 'stop',
-                frames: [0],
-            },
-            {
-                visibility: [0.3, 0.45],
-                type: 'stop',
-                frames: [0, 45],
-            },
-            {
-                visibility: [0.45, 1.0],
-                type: 'play',
-                frames: [0, 150],
+                visibility: [0.25, 0.75],
+                type: 'seek',
+                frames: [0, frames],
             },
         ],
-    };
-    const content = {
-        cultivate: {
-            id: `cultivate-text`,
-            heading: `Cultivate`,
-            body: `Convening diverse groups of policy experts and stakeholders to generate productive disagreement over policy challenges and germinate fresh solutions to those challenges.`,
-        },
-        incubate: {
-            id: `incubate-text`,
-            heading: `Incubate`,
-            body: `Leveraging the policy expertise of our Strategic Council, technical team, and campaign partners to transform novel policy ideas into actionable policy initiatives. `,
-        },
-        accelerate: {
-            id: `accelerate-text`,
-            heading: `Accelerate`,
-            body: `Building and executing campaigns to guide our initiatives through the necessary chanels of implementation, be they statuatory, regulatory, or at the agency level. `,
-        },
-    };
+    });
 
-    return (
-        <InitiativesWrapper className="initiative-container">
-            <div className="icons">
-                <Lottie
-                    animationData={icons}
-                    style={style}
-                    interactivity={interactivity}
-                />
-            </div>
-            <div className="cultivate">
-                <Lottie
-                    animationData={cultivate}
-                    interactivity={interactivity}
-                    style={style}
-                    loop={false}
-                />
-            </div>
-            <InitiativesText
-                id={content.cultivate.id}
-                heading={content.cultivate.heading}
-                body={content.cultivate.body}
-            />
-            <InitiativesText
-                id={content.incubate.id}
-                heading={content.incubate.heading}
-                body={content.incubate.body}
-            />
-            <div className="incubate">
-                <Lottie
-                    animationData={incubate}
-                    style={style}
-                    interactivity={interactivity}
-                    loop={false}
-                />
-            </div>
-            <div className="accelerate">
-                <Lottie
-                    animationData={accelerate}
-                    interactivity={interactivity}
-                    style={style}
-                    loop={false}
-                />
-            </div>
-            <InitiativesText
-                id={content.accelerate.id}
-                heading={content.accelerate.heading}
-                body={content.accelerate.body}
-            />
-        </InitiativesWrapper>
-    );
+    return Animation;
 }
