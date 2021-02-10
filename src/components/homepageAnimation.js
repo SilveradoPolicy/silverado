9db48620e21fa0a99352f62b79dc1bb71f330646
kcbreuil <kcbreuil@gmail.com>
2021-02-10 14:04:03 -0500

    text under animation

--- src/components/homepageAnimation.js
+++ src/components/homepageAnimation.js
@@ -6,7 +6,7 @@ import tw from 'twin.macro';
 import homepageAnimation from '../animations/Homepage.json';
 
 const AnimationFlex = styled.section`
-    ${tw`flex`}
+    ${tw`flex justify-center mr-2 ml-16 space-x-64 font-wt-bold text-brand-1 text-center text-ts-h2`}
 `;
 
 export default function HomepageAnimation() {
