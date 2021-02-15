11dd611aa6c16f0d64e38af2e8316896e031b251
kcbreuil <kcbreuil@gmail.com>
2021-02-15 16:07:29 -0500

    siteurl

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
     siteMetadata: {
         title: 'Silverado Policy Accelerator',
         titleTemplate: '%s',
-        url: 'https://www.silverado.org',
+        siteUrl: 'https://www.silverado.org',
         description: `Silverado is a think tank dedicated to securing enduring American prosperity and competitiveness by investing in innovative bipartisan policy solutions.`,
         image: '',
         twitter: '@SilveradoPolicy',
