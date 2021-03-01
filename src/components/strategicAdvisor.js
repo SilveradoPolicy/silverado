1bfbfbec648f55024ce47f14e237e020fd02507c
kcbreuil <kcbreuil@gmail.com>
2021-03-01 15:20:43 -0500

    modal

--- src/components/strategicAdvisor.js
+++ src/components/strategicAdvisor.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import Modal from 'react-modal';
 
 const StyledName = styled.h5`
     ${tw`capitalize text-brand-1 text-ts-h5`}
@@ -12,31 +13,79 @@ const StyledTitle = styled.p`
 `;
 
 const StyledDiv = styled.div`
-    ${tw`w-80 p-6 bg-opacity-100 bg-white rounded-lg shadow-lg absolute`}
+    ${tw`text-brand-1 p-4 bg-opacity-100 bg-white absolute`}
 `;
 
+const StyledModal = styled.p`
+    ${tw`text-black`}
+`;
+
+const customStyles = {
+    content: {
+        width: '364px',
+        height: '350px',
+        top: '60%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        background: '#FFFFFF',
+        boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.25)',
+        borderRadius: '23px',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
 export default function StrategicAdvisor({ data }) {
-    const [isShown, setIsShown] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const { advisorFullName, title } = data;
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        // subtitle.style.color = '#306184';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
     return (
-        <div
-            onMouseEnter={() => setIsShown(true)}
-            onMouseLeave={() => setIsShown(false)}
-        >
-            <StyledName className="font-wt-bold">{advisorFullName}</StyledName>
+        <div>
+            <button type="button" onClick={openModal} onKeyDown={openModal}>
+                <StyledName className="font-wt-bold">
+                    <label>{advisorFullName}</label>
+                </StyledName>
+            </button>
             <StyledTitle className="font-wt-thin">{title}</StyledTitle>
-            {isShown && (
+            <Modal
+                isOpen={isOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
                 <StyledDiv>
-                    Quisque consequat, massa ut convallis sagittis, ipsum lectus
-                    tempus urna, nec consequat massa leo sed metus. Morbi eget
-                    justo augue. Etiam a tortor ligula. Duis pharetra egestas
-                    urna eu maximus. Integer venenatis convallis elementum.
-                    Donec ullamcorper consequat nibh, ac sodales nunc vestibulum
-                    eget. Nam ultrices blandit velit at finibus. Nulla vitae
-                    condimentum risus. Sed vitae porta tortor. Nulla interdum
-                    sapien mi..
+                    <h1 className="font-wt-bold" style={{ fontSize: '23px' }}>
+                        {advisorFullName}
+                    </h1>
+                    <h2 className="font-wt-thin">{title}</h2>
+                    <br />
+                    <StyledModal>
+                        Quisque consequat, massa ut convallis sagittis, ipsum
+                        lectus tempus urna, nec consequat massa leo sed metus.
+                        Morbi eget justo augue. Etiam a tortor ligula. Duis
+                        pharetra egestas urna eu maximus. Integer venenatis
+                        convallis elementum. Donec ullamcorper consequat nibh,
+                        ac sodales nunc vestibulum eget.
+                    </StyledModal>
+                    <br />
+                    <button type="button" onClick={closeModal}>
+                        x
+                    </button>
                 </StyledDiv>
-            )}
+            </Modal>
         </div>
     );
 }
