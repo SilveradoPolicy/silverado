7a33e901df3132bb0859ef197753de58cae7d09c
Katie Breuil <61708936+kcbreuil@users.noreply.github.com>
2021-03-04 19:08:40 -0500

    Add bio modals
    * hover bio
    
    * modal
    
    * label update
    
    * positioning
    
    * move modal down
    
    * remove z index
    
    * update
    
    * edits
    
    * icon and pointer
    
    * more edits
    
    * remove amy edit
    
    * updates
    
    * role
    
    * edit modal logic
    
    * add modal
    
    * modal edits
    
    * modal edits
    
    * first part of bios added
    
    * bios
    
    * Move tailwind classes to styled-component definition
    
    * Put font classes back in className, because they're necessary
    
    * Update copy
    
    * Set modal maxWidth to 80%
    
    * Fix typo
    
    * Suppress bio modals on screens smaller than 768px
    
    * Add missing bios
    
    * Update copy
    
    * Remove text transform
    
    Co-authored-by: asasmith <smith.asa.la@gmail.com>

--- src/components/imageWithName.js
+++ src/components/imageWithName.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import Img from 'gatsby-image';
+import Modal from 'react-modal';
+import { MdClose } from 'react-icons/md';
 
 const FlexWrapper = styled.div`
     ${tw`flex flex-col items-center`}
@@ -20,8 +22,48 @@ const StyledTitle = styled.div`
     ${tw`text-brand-1`}
 `;
 
+const StyledDiv = styled.div`
+    ${tw`text-brand-1 p-4 bg-opacity-100 bg-white`}
+`;
+
+const StyledModal = styled.p`
+    ${tw`text-black`}
+`;
+
+const StyledButton = styled.button`
+    ${tw`top-2.5 right-2.5 absolute`}
+`;
+
+const ModalTitle = styled.h2`
+    ${tw`mb-4`}
+`;
+
+const ModalName = styled.h2`
+    ${tw`text-2xl`}
+`;
+
+const customStyles = {
+    content: {
+        maxWidth: '80%',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        background: '#FFFFFF',
+        boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.25)',
+        borderRadius: '23px',
+        transform: 'translate(-50%, -50%)',
+        position: 'relative',
+    },
+    overlay: {
+        zIndex: '10',
+    },
+};
+
 export default function ImageWithName({ data }) {
+    const [isOpen, setIsOpen] = useState(false);
     const {
+        bio,
         fullName,
         image: {
             childImageSharp: { fluid },
@@ -29,13 +71,50 @@ export default function ImageWithName({ data }) {
         title,
     } = data;
 
+    function toggleModal() {
+        const { innerWidth } = window;
+
+        if (innerWidth >= 768) {
+            Modal.setAppElement('#___gatsby');
+            setIsOpen(!isOpen);
+        }
+    }
+
+    function handleKeydown(e) {
+        const enterKeyWasPressed = e.keyCode === 13;
+
+        if (enterKeyWasPressed) {
+            toggleModal();
+        }
+    }
+
     return (
-        <FlexWrapper>
+        <FlexWrapper
+            role="button"
+            onClick={toggleModal}
+            onKeyDown={handleKeydown}
+            tabIndex={0}
+        >
             <ImgWrapper>
                 <Img fluid={fluid} />
             </ImgWrapper>
             <StyledName className="font-wt-bold">{fullName}</StyledName>
             <StyledTitle className="font-wt-reg">{title}</StyledTitle>
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={toggleModal}
+                style={customStyles}
+                contentLabel="Bio Modal"
+            >
+                <StyledDiv>
+                    <ModalName className="font-wt-bold">{fullName}</ModalName>
+                    <ModalTitle className="font-wt-thin">{title}</ModalTitle>
+                    <StyledModal>{bio}</StyledModal>
+                    <StyledButton role="button" onClick={toggleModal}>
+                        <MdClose />
+                    </StyledButton>
+                </StyledDiv>
+            </Modal>
         </FlexWrapper>
     );
 }
--- src/components/strategicAdvisor.js
+++ src/components/strategicAdvisor.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import Modal from 'react-modal';
+import { MdClose } from 'react-icons/md';
+
+const ButtonDiv = styled.div`
+    ${tw`cursor-pointer`}
+`;
 
 const StyledName = styled.h5`
     ${tw`text-brand-1 text-ts-h5`}
@@ -11,12 +17,97 @@ const StyledTitle = styled.p`
     ${tw`text-brand-1`}
 `;
 
+const StyledDiv = styled.div`
+    ${tw`text-brand-1 p-4 bg-opacity-100 bg-white`}
+`;
+
+const StyledModal = styled.p`
+    ${tw`text-black`}
+`;
+
+const StyledButton = styled.button`
+    ${tw`top-2.5 right-2.5 absolute`}
+`;
+
+const ModalTitle = styled.h2`
+    ${tw`mb-4`}
+`;
+
+const ModalName = styled.h2`
+    ${tw`text-2xl`}
+`;
+
+const customStyles = {
+    content: {
+        maxWidth: '80%',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        background: '#FFFFFF',
+        boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.25)',
+        borderRadius: '23px',
+        transform: 'translate(-50%, -50%)',
+        position: 'relative',
+    },
+    overlay: {
+        zIndex: '10',
+    },
+};
+
 export default function StrategicAdvisor({ data }) {
-    const { advisorFullName, title } = data;
+    const [isOpen, setIsOpen] = useState(false);
+    const { advisorFullName, bio, title } = data;
+
+    function toggleModal() {
+        const { innerWidth } = window;
+
+        if (innerWidth >= 768) {
+            Modal.setAppElement('#___gatsby');
+            setIsOpen(!isOpen);
+        }
+    }
+
+    function handleKeydown(e) {
+        const enterKeyWasPressed = e.keyCode === 13;
+
+        if (enterKeyWasPressed) {
+            toggleModal();
+        }
+    }
+
     return (
         <div>
-            <StyledName className="font-wt-bold">{advisorFullName}</StyledName>
-            <StyledTitle className="font-wt-reg">{title}</StyledTitle>
+            <ButtonDiv
+                role="button"
+                onClick={toggleModal}
+                onKeyDown={handleKeydown}
+                tabIndex={0}
+            >
+                <StyledName className="font-wt-bold">
+                    {advisorFullName}
+                </StyledName>
+                <StyledTitle className="font-wt-thin">{title}</StyledTitle>
+                <Modal
+                    isOpen={isOpen}
+                    onRequestClose={toggleModal}
+                    style={customStyles}
+                    contentLabel="Bio Modal"
+                >
+                    <StyledDiv>
+                        <ModalName className="font-wt-bold">
+                            {advisorFullName}
+                        </ModalName>
+                        <ModalTitle className="font-wt-thin">
+                            {title}
+                        </ModalTitle>
+                        <StyledModal>{bio}</StyledModal>
+                        <StyledButton role="button" onClick={toggleModal}>
+                            <MdClose />
+                        </StyledButton>
+                    </StyledDiv>
+                </Modal>
+            </ButtonDiv>
         </div>
     );
 }
