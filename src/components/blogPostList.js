c12402895bd809bf84f3fb375ce113da292de3a5
Katie Breuil <61708936+kcbreuil@users.noreply.github.com>
2021-04-08 19:55:35 -0400

    Blog post list component (#146)
    * WIP on blog post list component
    
    * ready for review
    
    * underline styling for active link
    
    * edit

---
+++ src/components/blogPostList.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+import BlogCard from './blogCard';
+
+const BlogPostsWrapper = styled.section`
+    ${tw`container mt-20`}
+`;
+
+const PostWrapper = styled.div`
+    ${tw`flex gap-4 relative`}
+`;
+
+const LinkWrapper = styled.div`
+    ${tw`space-x-8`}
+`;
+const StyledLink = styled(Link)`
+    ${tw`text-ts-h5`}
+    text-decoration: ${({ isActive }) => (isActive ? 'underline' : 'none')};
+`;
+
+export default function BlogPostList({ blogposts }) {
+    const [activeLink, setActiveLink] = useState(null);
+    const pillars = [
+        {
+            title: 'All',
+            id: '01',
+        },
+        {
+            title: 'Eco²Sec',
+            id: '02',
+        },
+        {
+            title: 'International Trade & Security',
+            id: '03',
+        },
+        {
+            title: 'Cybersecurity',
+            id: '04',
+        },
+    ];
+    return (
+        <BlogPostsWrapper>
+            <LinkWrapper>
+                {pillars.map((pillar) => {
+                    return (
+                        <StyledLink
+                            key={pillar.id}
+                            to="#"
+                            className="font-wt-bold"
+                            onClick={() => setActiveLink(pillar.id)}
+                            isActive={activeLink === pillar.id}
+                        >
+                            {pillar.title}
+                        </StyledLink>
+                    );
+                })}
+            </LinkWrapper>
+            <PostWrapper>
+                {blogposts.map((post) => {
+                    return <BlogCard data={post} />;
+                })}
+            </PostWrapper>
+        </BlogPostsWrapper>
+    );
+}
+
+BlogPostList.propTypes = {
+    blogposts: PropTypes.array.isRequired,
+};
