babcfe7048d274e16b66cf191f40dfc7b1907365
asasmith <smith.asa.la@gmail.com>
2021-04-20 18:37:14 -0400

    Blog Template  (#147)
    * WIP blog template
    
    * finished template
    
    * card update
    
    * check
    
    * check card
    
    * update
    
    * update cardlist
    
    * edits
    
    * add flex wrap

---
+++ web/src/components/blogHero.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const HeroWrapper = styled.div`
+    ${tw`mx-8 pt-36`}
+`;
+const StyledImg = styled(Img)`
+    ${tw`mt-4`}
+    &:before {
+        background: var(--brand-4);
+        content: '';
+        height: 20px;
+        position: absolute;
+        width: 100%;
+        top: 0;
+        left: 0;
+        z-index: 2;
+    }
+`;
+const StyledLink = styled(Link)`
+    ${tw`pb-10 text-brand-1`}
+`;
+export default function BlogHero({ image }) {
+    const {
+        childImageSharp: { fluid },
+    } = image;
+    return (
+        <HeroWrapper>
+            <StyledLink to="/news"> {'<'} Back to News</StyledLink>
+            <StyledImg fluid={fluid} />
+        </HeroWrapper>
+    );
+}
+
+export const query = graphql`
+    query BlogHeroQuery {
+        silverado: file(relativePath: { regex: "/Silverado1/" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+            id
+        }
+    }
+`;
+
+BlogHero.propTypes = {
+    image: PropTypes.object.isRequired,
+};
