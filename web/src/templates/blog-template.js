f9155d5cccd19603f06f919a99e787e1b51e0916
asasmith <smith.asa.la@gmail.com>
2021-04-29 10:52:52 -0400

    Move blog-template to templates dir

--- web/src/templates/blog-template.js
+++ web/src/templates/blog-template.js
@@ -4,15 +4,43 @@ import PropTypes from 'prop-types';
 
 import Layout from '../layouts/page-layout';
 import BlogHero from '../components/blogHero';
-
 import BlogContent from '../components/blogContent';
 import PillarCardList from '../components/pillarCardList';
 
+export const query = graphql`
+    query newsBlogTemplateQuery($id: String!) {
+        post: sanityPost(id: { eq: $id }) {
+            categories {
+                color
+                id
+                name
+            }
+            heroImage {
+                asset {
+                    fluid(maxWidth: 2000) {
+                        ...GatsbySanityImageFluid
+                    }
+                }
+            }
+            id
+            postTitle
+            _rawBody(resolveReferences: { maxDepth: 10 })
+        }
+    }
+`;
+
 export default function BlogTemplate({ data }) {
-    const { silverado } = data;
+    const {
+        post: { categories, heroImage, postTitle, _rawBody },
+    } = data;
+
+    const { color } = categories[0];
+
+    const postData = { categories, postTitle, _rawBody };
+
     const posts = [
         {
-            image: silverado,
+            image: heroImage,
             title: 'This is an event',
             subtitle: 'Cras iaculis',
             time: '12:30pm',
@@ -22,7 +50,7 @@ export default function BlogTemplate({ data }) {
         },
         {
             id: '02',
-            image: silverado,
+            image: heroImage,
             title: 'This is an event',
             subtitle: 'Cras iaculis',
             description:
@@ -30,7 +58,7 @@ export default function BlogTemplate({ data }) {
         },
         {
             id: '03',
-            image: silverado,
+            image: heroImage,
             title: 'This is an event',
             subtitle: 'Cras iaculis',
             description:
@@ -39,28 +67,14 @@ export default function BlogTemplate({ data }) {
     ];
     return (
         <Layout hasBackgroundColor>
-            <BlogHero image={silverado} />
-            <BlogContent />
+            <BlogHero color={color} image={heroImage} />
+            <BlogContent data={postData} />
             <PillarCardList list={posts} />
         </Layout>
     );
 }
-
-export const query = graphql`
-    query BlogTemplateQuery {
-        silverado: file(relativePath: { regex: "/Silverado1/" }) {
-            childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid
-                }
-            }
-            id
-        }
-    }
-`;
-
 BlogTemplate.propTypes = {
     data: PropTypes.shape({
-        silverado: PropTypes.object.isRequired,
+        post: PropTypes.object.isRequired,
     }).isRequired,
 };
