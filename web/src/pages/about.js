69af2b6f77d8c03e1598667cda493ac878bef7e6
kcbreuil <kcbreuil@gmail.com>
2021-05-20 12:39:11 -0400

    haleySq

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -188,7 +188,7 @@ export const query = graphql`
             }
             id
         }
-        haley: file(relativePath: { regex: "/haley/" }) {
+        haley: file(relativePath: { regex: "/haleySq/" }) {
             childImageSharp {
                 fluid(maxWidth: 400) {
                     ...GatsbyImageSharpFluid
